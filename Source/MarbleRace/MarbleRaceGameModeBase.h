496c72806e0afb4a881426dc5035eebaa2752f02
Leo Stone <leonardo.rocha@gamecan.eu>
2024-02-29 17:50:00 +0200

    Initial commit

---
+++ Source/MarbleRace/MarbleRaceGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MarbleRaceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MARBLERACE_API AMarbleRaceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
